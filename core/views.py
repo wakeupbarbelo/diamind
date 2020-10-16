cb5a616bec3e2e4bd7144efaead3fffb309f99d8
Wake Up Barbélo <wakeupbarbelo@wakeupbarbelo.net>
2020-10-16 22:16:42 +0200

    HttpResponseForbidden fix.

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.contrib import messages
 from django.utils.html import mark_safe
+from django.http import HttpResponseForbidden
 
 from dal import autocomplete
 
